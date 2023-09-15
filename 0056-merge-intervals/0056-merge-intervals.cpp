320bfb664d385a81e350a16a44adef05b200322d
Muhammad Arslan <129847284+Arslanarsal@users.noreply.github.com>
2023-09-15 21:11:36 +0500

    Time: 34 ms (41.77%), Space: 19.3 MB (28.94%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+            if (intervals.size() == 0 || intervals.size() == 1)
+    {
+        return intervals;
+    }
+    sort(intervals.begin(), intervals.end());
+    vector<vector<int>> ans;
+    ans.push_back(intervals[0]);
+    for (int i = 1; i < intervals.size(); i++)
+    {
+        if (ans[ans.size() - 1][1] >= intervals[i][0])
+        {
+              if (ans[ans.size() - 1][1] < intervals[i][1])
+            {
+                ans[ans.size() - 1][1] = intervals[i][1];
+            }
+        }
+        else
+        {
+            ans.push_back(intervals[i]);
+        }
+    }
+    return ans;
+    }
+};
