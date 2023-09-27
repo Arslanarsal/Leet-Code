e08e0028a3a73eacebd01369049f54ef8f0bb64a
Muhammad Arslan <129847284+Arslanarsal@users.noreply.github.com>
2023-09-27 12:29:37 +0500

    Time: 3 ms (20.02%), Space: 6.2 MB (13.76%) - LeetHub

---
+++ 2050-count-good-numbers/2050-count-good-numbers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+long long mod = 1e9 + 7;
+    long long powe(long long x, long long n)
+{
+    if (!n)
+    {
+        return 1;
+    }
+    long long temp = powe(x, n / 2);
+    if (n % 2 != 0)
+    {
+        return (x * temp * temp) % mod;
+    }
+    return (temp * temp) % mod;
+}
+
+int countGoodNumbers(long long n)
+{
+
+    long long  even = (n + 1) / 2;
+    long long  odd = n / 2;
+
+    long long first = (powe(5, even) % mod);
+    long long second = (powe(4, odd) % mod);
+
+    return (int)((first * second) % mod);
+}
+};
