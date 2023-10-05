9fbe5ea914892245eda6d61189fe3aae641168d6
Muhammad Arslan <129847284+Arslanarsal@users.noreply.github.com>
2023-10-05 11:58:28 +0500

    Time: 14 ms (18.11%), Space: 16.3 MB (18.73%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,52 +1,26 @@
 class Solution {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
-            int ele1, ele2, count1 = 0, count2 = 0;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (count1 == 0 && ele2 != nums[i])
-        {
-            count1=1, ele1 = nums[i];
-        }
-        else if (count2 == 0 && ele1 != nums[i])
-        {
-            count2=1, ele2 = nums[i];
-        }
-        else if (ele1 == nums[i])
-        {
-            count1++;
-        }
-        else if (ele2 == nums[i])
-        {
-            count2++;
-        }
-        else
-        {
-            count1--;
-            count2--;
-        }
-    }
-    count1 = 0;
-    count2 = 0;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (ele1 == nums[i])
-        {
-            count1++;
-        }if (ele2 == nums[i])
-        {
-            count2++;
-        }
-    }
-    vector<int> ans;
-    if (count1 > nums.size() / 3)
-    {
-        ans.push_back(ele1);
-    }
-    if (count2 > nums.size() / 3)
-    {
-        ans.push_back(ele2);
-    }
-    return ans;
-    }
+  vector<int> majorityElement(vector<int> &a) 
+  {
+	  int y(-1), z(-1), cy(0), cz(0);
+    
+	  for (const auto & x: a) 
+	  {
+		  if (x == y) cy++;
+		  else if (x == z) cz++;
+		  else if (! cy) y = x, cy = 1;
+		  else if (! cz) z = x, cz = 1;
+		  else cy--, cz--;
+     }
+      
+     cy = cz = 0;
+     for (const auto & x: a)
+		 if (x == y) cy++;
+			 else if (x == z) cz++;
+  
+	  vector<int> r;
+	  if (cy > size(a)/3) r.push_back(y);
+	  if (cz > size(a)/3) r.push_back(z);
+	  return r;
+  }
 };
