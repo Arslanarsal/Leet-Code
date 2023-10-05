18a9ae55f2baa299ec761e741d1be84ce85cbe5e
Muhammad Arslan <129847284+Arslanarsal@users.noreply.github.com>
2023-10-05 11:16:37 +0500

    Time: 0 ms (100%), Space: 6.4 MB (10.85%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-         return __builtin_popcount(start ^ goal);
+       int flip_count = 0;
+
+    // XOR start and goal to find bits that need flipping
+    int flip_bits = start ^ goal;
+
+    // Count number of bits that need flipping using built-in function
+    flip_count = __builtin_popcount(flip_bits);
+
+    return flip_count;
     }
 };
