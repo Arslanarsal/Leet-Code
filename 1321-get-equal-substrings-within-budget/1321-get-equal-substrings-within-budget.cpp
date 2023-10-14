6608bf5d0530f2a14e3420041773542d0c645559
Muhammad Arslan <129847284+Arslanarsal@users.noreply.github.com>
2023-10-14 16:09:09 +0500

    Time: 4 ms (86.77%), Space: 8.1 MB (28.77%) - LeetHub

---
+++ 1321-get-equal-substrings-within-budget/1321-get-equal-substrings-within-budget.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+          int head, tail = 0, ans = 0, cost = 0;
+    for (head = 0; head < s.size(); head++)
+    {
+        cost += abs(s[head] - t[head]);
+        while (cost > maxCost)
+        {
+            cost = cost - abs(s[tail] - t[tail]);
+            tail++;
+        }
+        ans = max(ans, head - tail + 1);
+    }
+    return ans;
+    }
+};
