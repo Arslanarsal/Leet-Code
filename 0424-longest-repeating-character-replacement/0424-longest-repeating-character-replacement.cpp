0f798c7908a8b8873dd9305a03c9d9c4acd72591
Muhammad Arslan <129847284+Arslanarsal@users.noreply.github.com>
2023-10-14 19:36:59 +0500

    Time: 6 ms (84.42%), Space: 7.3 MB (32.52%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
-        int head, tail = 0, ans = 0, A[26] = {};
-    for (head = 0; head < s.length(); head++)
+    int characterReplacement(string &str, int k) {
+      int ans = 0;          // Stores the length of the longest repeating substring
+    int n = str.length(); // Get the size of the input string
+    int i = 0, j;         // 'i' points to the start of the window, and 'j' points to the end of the window
+    int cnt[26] = {0};    // Stores the count of characters (A-Z)
+    int mx = 0;           // Variable to store the maximum count of characters in the current substring
+
+    for (j = 0; j < n; j++)
     {
-        A[s[head] - 'A']++;
-        while (head - tail + 1 - *max_element(A, A + 26) > k)
+        int x = str[j] - 'A'; // Convert the character to its corresponding index (0-25) in the 'cnt' array
+        cnt[x]++;             // Increment the count of the current character
+        mx = max(mx, cnt[x]); // Update the maximum count of characters in the current substring
+
+        // Slide the window (from left) until the number of character changes allowed 'k' is satisfied
+        while ((j - i + 1 - mx) > k)
         {
-            A[s[tail++] - 'A']--;
+            int y = str[i] - 'A'; // Convert the character to its corresponding index (0-25) in the 'cnt' array
+            cnt[y]--;             // Decrement the count of the character at the start of the window
+            i++;                  // Move the start of the window to the right
         }
-        ans = max(ans, head - tail + 1);
+
+        // Update the length of the longest repeating substring found so far
+        ans = max(ans, j - i + 1);
     }
+
+    // Return the length of the longest repeating substring
     return ans;
     }
 };
